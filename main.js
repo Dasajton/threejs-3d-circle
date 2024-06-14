007223b91fcbfc887d0dbbd9a6fb0c099ea7777c
Dasajton <david.sajitz.94@gmail.com>
2024-06-14 12:56:06 +0200

    resizable circle with scene, sphere, light, camera and rendering on canvas screen

--- main.js
+++ main.js
@@ -1,7 +1,55 @@
+import * as THREE from "three";
 import "./style.scss";
 
-document.querySelector("#app").innerHTML = /*html*/ `
-  <div>
-    <h1>Hello ThreeJS</h1>
-  </div>
-`;
+// Scene
+const scene = new THREE.Scene();
+
+// Create the Sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+  color: "#0ea5e9",
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// Light
+const light = new THREE.PointLight(0xffffff, 100, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 20;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+// Resize
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  camera.updateProjectionMatrix();
+  camera.aspect = sizes.width / sizes.height;
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const loop = () => {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+loop();
